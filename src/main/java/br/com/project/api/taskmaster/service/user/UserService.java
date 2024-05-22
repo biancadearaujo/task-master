a9664f715d81e2225341b98ed5f6f6f7706b2f79
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2024-05-22 12:58:25 -0300

    refactor: user service

--- src/main/java/br/com/project/api/taskmaster/service/user/UserService.java
+++ src/main/java/br/com/project/api/taskmaster/service/user/UserService.java
@@ -1,43 +1,47 @@
 package br.com.project.api.taskmaster.service.user;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.project.api.taskmaster.command.CreateUserCommand;
+import br.com.project.api.taskmaster.exception.user.InvalidUserException;
 import br.com.project.api.taskmaster.exception.user.NoRegisteredUsersException;
-import br.com.project.api.taskmaster.exception.user.UserAlreadyExistsException;
 import br.com.project.api.taskmaster.model.user.UserModel;
 import br.com.project.api.taskmaster.repository.user.UserRepository;
 import br.com.project.api.taskmaster.utils.user.GenerationTimestamp;
+import br.com.project.api.taskmaster.validator.user.UserValidator;
 import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	final UserRepository userRepository;
 	final UserCreator userCreator;
 	final UserUpdater userUpdater;
-	final GenerationTimestamp generationTimestamp;
+	final UserValidator userValidator;
 	
 	public UserService(UserRepository userRepository, UserCreator userCreator, UserUpdater userUpdater,
-			GenerationTimestamp generationTimestamp) {
+			GenerationTimestamp generationTimestamp, UserValidator userValidator) {
 		this.userRepository = userRepository;
 		this.userCreator = userCreator;
 		this.userUpdater = userUpdater;
-		this.generationTimestamp = generationTimestamp;
+		this.userValidator = userValidator;
 	}
 
-	//não esta buscando os usuarios, apenas esta mostrando os dados da paginação
 
 	public Page<UserModel> getAllUsers(Pageable pageable) {
     	
-    	return userRepository.findAll(pageable);
-    	
+    	return userRepository.findAll(pageable);	
     }
 	
 	public UserModel getUserById(int id) {
@@ -48,26 +52,48 @@ public class UserService {
     @Transactional
 	public UserModel register(CreateUserCommand createUserCommand){
 		
-      /*  if(this.userRepository.findByLogin(createUserCommand.getLogin()) != null) {
-        	throw new UserAlreadyExistsException("Existing user with this login");
-        }
-        */
-       UserModel newUser = userCreator.createUserAttributes(createUserCommand);
-        
-        return userRepository.save(newUser);
-        
+    	try {
+    		List<String> validationErrors = userValidator.validateUser(createUserCommand);
+    		
+    		if(!validationErrors.isEmpty()) {
+    			throw new InvalidUserException(validationErrors);
+    		}
+    		
+    		UserModel newUser = userCreator.createUserAttributes(createUserCommand);
+    		return userRepository.save(newUser);
+			
+		} catch (InvalidUserException e) {
+			logger.error("Validation error for the registering user", e);
+			throw e;
+		}catch (Exception e) {
+			logger.error("Error registering user", e);
+	        throw new RuntimeException("Error registering user", e);
+		}   
 	}
 	
 	
     @Transactional
 	public UserModel updateUser(UUID userId, Map<String, Object> updateAttributes) {
-	    //Trocar por Optional
-	    UserModel userModel = userRepository.findById(userId).orElseThrow(() -> new NoRegisteredUsersException("User not found"));
-	    
-	    userUpdater.updateUserAttributes(updateAttributes, userModel);
+    	
+    	try {
+    		List<String> validationErrors = userValidator.validateUserUpdate(userId, updateAttributes);
+    		
+    		if(!validationErrors.isEmpty()) {
+    			throw new InvalidUserException(validationErrors);
+    		}
+    		UserModel userModel = userRepository.findById(userId).orElseThrow(() -> new NoRegisteredUsersException("User not found"));
+    		
+    		userUpdater.updateUserAttributes(updateAttributes, userModel);
 
-	    return userRepository.save(userModel);
-	    
+    	    return userRepository.save(userModel); 
+			
+		} catch (InvalidUserException e) {
+			logger.error("Validation error for the registering user", e);
+			throw e;
+		} catch (Exception e) {
+			logger.error("Validation error when updating user", e);
+	        throw new RuntimeException("Error updating user", e);
+		}
 	}
 
 	
@@ -82,6 +108,5 @@ public class UserService {
 		
 		return"User successfully deleted";
 		
-	}
-	
+	}	
 }
