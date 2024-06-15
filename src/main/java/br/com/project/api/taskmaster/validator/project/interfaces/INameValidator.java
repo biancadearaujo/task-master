b2bdad24a98a462d185a7e7d0fcaa984f6dbefe2
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2024-06-15 19:38:25 -0300

    feat: I name validator

---
+++ src/main/java/br/com/project/api/taskmaster/validator/project/interfaces/INameValidator.java
@@ -0,0 +1,10 @@
+package br.com.project.api.taskmaster.validator.project.interfaces;
+
+import java.util.List;
+
+public interface INameValidator {
+
+	List<String> NameIsValid(String name);
+	static final int MAXIMUM_SIZE = 100;
+	static final int MINIMUM_SIZE = 0;
+}
