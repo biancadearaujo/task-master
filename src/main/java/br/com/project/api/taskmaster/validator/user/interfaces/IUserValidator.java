669150a57b90a908d61b8a6c093ac1dc2742d274
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2024-06-15 19:04:29 -0300

    chore: user validator

--- src/main/java/br/com/project/api/taskmaster/validator/user/interfaces/IUserValidator.java
+++ src/main/java/br/com/project/api/taskmaster/validator/user/interfaces/IUserValidator.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import br.com.project.api.taskmaster.command.CreateUserCommand;
+import br.com.project.api.taskmaster.command.user.CreateUserCommand;
 
 public interface IUserValidator {
 
