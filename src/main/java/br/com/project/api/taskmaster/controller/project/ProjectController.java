60b623ba2db5630f37c28282b1c87c5e8d24d250
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2024-07-16 21:53:20 -0300

    feat: project controller

--- src/main/java/br/com/project/api/taskmaster/controller/project/ProjectController.java
+++ src/main/java/br/com/project/api/taskmaster/controller/project/ProjectController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,6 +72,7 @@ public class ProjectController {
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasRole('ADMIN') or #userId == authentication.principal.idUser")
 	public ResponseEntity<Object> save(CreateProjectCommand createProjectCommand) {
 		try {
 			List<String> validationErrors = projectValidator.validateProject(createProjectCommand);
