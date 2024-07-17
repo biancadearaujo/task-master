e357e58fcb4beb9dbe9d689c6ee48fd2351e0044
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2024-07-16 21:53:59 -0300

    refactor: project creator

--- src/main/java/br/com/project/api/taskmaster/service/project/ProjectCreator.java
+++ src/main/java/br/com/project/api/taskmaster/service/project/ProjectCreator.java
@@ -1,10 +1,13 @@
 package br.com.project.api.taskmaster.service.project;
 
+import org.springframework.stereotype.Component;
+
 import br.com.project.api.taskmaster.builder.project.ProjectBuilder;
 import br.com.project.api.taskmaster.command.project.CreateProjectCommand;
 import br.com.project.api.taskmaster.model.project.ProjectModel;
 import br.com.project.api.taskmaster.utils.user.GenerationTimestamp;
 
+@Component
 public class ProjectCreator {
 
 	private final GenerationTimestamp generationTimestamp;
