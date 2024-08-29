a14fc3eb6d9de7a1c7942f5ac458d490553129dc
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2024-08-29 13:57:37 -0300

    feat: ProjectRepository

--- src/main/java/br/com/project/api/taskmaster/repository/project/ProjectRepository.java
+++ src/main/java/br/com/project/api/taskmaster/repository/project/ProjectRepository.java
@@ -1,5 +1,6 @@
 package br.com.project.api.taskmaster.repository.project;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,9 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.project.api.taskmaster.model.project.ProjectModel;
+import br.com.project.api.taskmaster.model.user.UserModel;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectModel, UUID>{
 
 	Optional<ProjectModel>findByName(String name);
+	List<ProjectModel> findByUser(UserModel userModel);
 }
